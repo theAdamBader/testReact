73b57e6cb8a89e083130e3b9068199698832c764
Adam Bader <adambader25@gmail.com>
2020-09-13 14:00:05 +0100

    changing and more

--- fun_zone/src/components/swiper/swiper.js
+++ fun_zone/src/components/swiper/swiper.js
@@ -1,30 +1,6 @@
-import React from "react";
-// import { Swiper, SwiperSlide } from 'swiper/react';
-// import SwiperCore from "swiper";
-// import 'swiper/swiper.scss';
-
-// const Slider = props => {
-//     const slides = [];
-
-//     for (let i = 0; i < slides.length; i++){
-//         slides.push(
-//             <SwiperSlide key = {'slide-${(i)}'}>
-//                 <img src={require("../../img/por.jpg")} alt=""/>
-//             </SwiperSlide>
-//         );
-//     }
-
-//     return <React.Fragment>
-//         <Swiper id = "gallerySwipe">
-//             {slides}
-//         </Swiper>
-//     </React.Fragment>
-// }
-
-// export default Slider;
-
-import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
-
+// Import React/SwiperCores/CSS
+import React, {useState} from "react";
+import SwiperCore, { Navigation, Thumbs } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
@@ -32,28 +8,50 @@ import 'swiper/swiper.scss';
 import 'swiper/components/navigation/navigation.scss';
 import 'swiper/components/pagination/pagination.scss';
 import 'swiper/components/scrollbar/scrollbar.scss';
+import '../../css/img.css';
 
 // install Swiper components
-SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+SwiperCore.use([Navigation, Thumbs]);
 
-const sliding = props => {
-  return (
-    <Swiper
-      spaceBetween={50}
-      slidesPerView={3}
-      navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-    >
-      <SwiperSlide><img src={require("../../img/gallery/0.jpg")} alt=""/></SwiperSlide>
-      <SwiperSlide><img src={require("../../img/gallery/1.jpg")} alt=""/></SwiperSlide>
-      <SwiperSlide><img src={require("../../img/gallery/2.jpg")} alt=""/></SwiperSlide>
-      <SwiperSlide><img src={require("../../img/gallery/3.jpg")} alt=""/></SwiperSlide>
-      ...
-    </Swiper>
-  );
+const Gallery = () => {
+    const [thumbsSwiper, setThumbsSwiper] = useState(null);
+
+    return (
+        <React.Fragment>
+            <div className = "galleryPlacement">
+                <h1>GALLERY</h1>
+                <Swiper
+                    thumbs={{swiper : thumbsSwiper}}
+                    spaceBetween= {10}
+                    loop= {true}
+                    loopedSlides= {5}
+                    navigation
+                    >
+                    <SwiperSlide><img className = "galleryImages" src={require("../../img/gallery/0.jpg")} alt=""/></SwiperSlide>
+                    <SwiperSlide><img className = "galleryImages" src={require("../../img/gallery/1.jpg")} alt=""/></SwiperSlide>
+                    <SwiperSlide><img className = "galleryImages" src={require("../../img/gallery/2.jpg")} alt=""/></SwiperSlide>
+                    <SwiperSlide><img className = "galleryImages" src={require("../../img/gallery/3.jpg")} alt=""/></SwiperSlide>
+                    <SwiperSlide><img className = "galleryImages" src={require("../../img/gallery/4.jpg")} alt=""/></SwiperSlide>
+
+                </Swiper>
+
+                <Swiper 
+                    onSwiper = {setThumbsSwiper}
+                    spaceBetween= {10}
+                    slidesPerView= {4}
+                    freeMode= {true}
+                    >
+
+                    <SwiperSlide><img className = "thumbImages" src={require("../../img/gallery/0.jpg")} alt=""/></SwiperSlide>
+                    <SwiperSlide><img className = "thumbImages" src={require("../../img/gallery/1.jpg")} alt=""/></SwiperSlide>
+                    <SwiperSlide><img className = "thumbImages" src={require("../../img/gallery/2.jpg")} alt=""/></SwiperSlide>
+                    <SwiperSlide><img className = "thumbImages" src={require("../../img/gallery/3.jpg")} alt=""/></SwiperSlide>
+                    <SwiperSlide><img className = "thumbImages" src={require("../../img/gallery/4.jpg")} alt=""/></SwiperSlide>
+
+                </Swiper>
+            </div>
+        </React.Fragment>
+    );
 };
 
-export default sliding;
+export default Gallery;
